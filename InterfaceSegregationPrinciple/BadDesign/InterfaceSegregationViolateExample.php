122e0998671270762ad2d64f494697d91c329796
Nahidul Hasan <nahidul.cse@gmail.com>
2018-05-16 17:20:45 +0600

    Added InterfaceSegregationPrinciple

---
+++ InterfaceSegregationPrinciple/BadDesign/InterfaceSegregationViolateExample.php
@@ -0,0 +1,31 @@
+<?php
+interface workerInterface{
+    public  function work();
+    public  function  sleep();
+}
+
+
+class HumanWorker implements workerInterface{
+
+    public  function work()
+    {
+        var_dump('works');
+    }
+    public  function  sleep()
+    {
+        var_dump('sleep');
+    }
+
+}
+
+class AndroidWorker implements workerInterface{
+
+    public  function work()
+    {
+        var_dump('works');
+    }
+    public  function sleep()
+    {
+        // No need
+    }
+}
---
+++ InterfaceSegregationPrinciple/GoodDesign/InterfaceSegregationExample.php
@@ -0,0 +1,31 @@
+<?php
+interface WorkAbleInterface{
+    public  function work();
+}
+
+interface SleepAbleInterface{
+    public  function  sleep();
+}
+
+
+class HumanWorker implements WorkAbleInterface, SleepAbleInterface{
+
+    public  function work()
+    {
+        var_dump('works');
+    }
+    public  function  sleep()
+    {
+        var_dump('sleep');
+    }
+
+}
+
+class AndroidWorker implements WorkAbleInterface{
+
+    public  function work()
+    {
+        var_dump('works');
+    }
+
+}
